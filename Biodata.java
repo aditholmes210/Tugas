a403847a81a0afbd8727f07f0b7f3adb1b4e3b3b
Aditya Anantama Sulaikan <adit.sulaikan@gmail.com>
2019-12-06 15:22:14 +0700

    update

--- Biodata.java
+++ Biodata.java
@@ -1,16 +1,16 @@
 public class Biodata{
-public static void main(String[] args){
-	String nama = "Aditya Anantama Sulaikan";
-	String tempat = "Jakarta";
-	int tanggal = 28;
-	int bulan = 09;
-	int tahun = 1994;
-	double umur = 25.16;
-	String alamat = "Jl. Tanjung I blok K3 no. 4 Sektor 1.1, BSD";
-	
-	System.out.println("Nama : "+nama);
-	System.out.println("TTL : "+tempat, +tanggal - +bulan - +tahun);
-	System.out.println("Umur : "+umur);
-	System.out.println("Alamat : "+alamat);
- }
+	public static void main(String[] args){
+		String nama = "Aditya Anantama Sulaikan";
+		String tempat = "Jakarta";
+		int tanggal = 28;
+		int bulan = 9;
+		int tahun = 1994;
+		double umur = 25.16;
+		String alamat = "Jl. Tanjung I blok K3 no. 4 Sektor 1.1, BSD";
+		
+		System.out.println("Nama : "+nama);
+		System.out.println("Tempat, Tanggal Lahir : "+tempat +"," +tanggal +"-" +bulan +"-" +tahun);
+		System.out.println("Umur : "+umur);
+		System.out.println("Alamat : "+alamat);
+	}
 }

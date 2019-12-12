3b9f704c31b690b2b69c149f4eab25de59d9202e
Aditya Anantama Sulaikan <adit.sulaikan@gmail.com>
2019-12-12 14:14:52 +0700

    edit looping

--- Looping.java
+++ Looping.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;  //import class
 public class Looping{
 	public static void print(Object obj){
 			System.out.println(obj);
@@ -25,10 +26,57 @@ public class Looping{
 			}
 		}*/
 		
-		//Nested for
+		/*Nested for
 		for(int i = 0; i < 5; i++){
 			for(int j = 0; j < 5; j++){
 				print("i ke-"+i+" dan j ke-"+j);
+			}
+		}*/
+		
+		/*For Each
+		int[] data = {-3,5,8,3,5};
+		int index = 0;
+		for(int i : data){
+			print("data ke "+index+" adalah "+i);
+			index++;
+		}*/
+		
+		/*While
+		int ageLeft = 10;
+		while(ageLeft > 0){				//}
+			print("Still alive !");
+			ageLeft--;
+			if(ageLeft == 0){			//}  Do
+				print("DIEE!!!");
+			}
+		}		*/						//}
+		
+		/*Do - While
+		int batt = 0;
+		do{
+			print("Turn power on");
+			batt--;
+		}
+		while(batt > 0);*/
+		
+		//membuat input dg scanner
+		boolean run = true;
+		while(run == true){
+			Scanner input = new Scanner(System.in);
+			print("Pilih 1 untuk lanjut running app");
+			print("Pilih 2 untuk lanjut running app");
+			print("Pilih 3 untuk stop app");
+			int x = input.nextInt();
+			switch(x){
+				case 1:
+				print("Kamu pilih 1");
+				break;
+				case 2:
+				print("Kamu pilih 2");
+				break;
+				default:
+				run = false;
+				break;
 			}
 		}
 	}
